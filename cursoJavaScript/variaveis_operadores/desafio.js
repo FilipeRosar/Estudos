3253aefb4991a8e64e446d15c3e444529ea3bfde
Filipe Rosa <filiperosa0312@gmail.com>
2023-11-24 01:56:09 -0300

    Criação curso JavaScript

---
+++ cursoJavaScript/variaveis_operadores/desafio.js
@@ -0,0 +1,8 @@
+const precoCombustivel = 5.79;
+const gastoCombustivel = 11;
+const distanciaEmKm = 60;
+
+const litrosConsumidos = distanciaEmKm / gastoCombustivel;
+const valorGasto = litrosConsumidos * precoCombustivel;
+
+console.log(valorGasto);
