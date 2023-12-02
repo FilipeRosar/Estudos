29685fe35fcd61a648a534ae32c6e23b1c5b7727
Filipe Rosa <filiperosa0312@gmail.com>
2023-12-02 15:38:34 -0300

    Update exercicios Js

---
+++ cursoJavaScript/modulo3/variaveis_escopos/aula1.js
@@ -0,0 +1,23 @@
+// var var1 = 10;
+// var var2 = '20';
+
+// console.log(var1 - var2)
+
+// var teste1 = function(){
+//     console.log('Primeiro Sucesso')
+// }
+
+
+// function teste2(){
+//     console.log('Segundo Sucesso');
+// }
+
+// teste1();
+// teste2();
+
+var var1;
+
+if (true){
+    var1 = 10;
+    console.log(var1)
+}
